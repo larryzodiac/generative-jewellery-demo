8550c2fc1b08e3b92b553b999eaeadabf58a0d34
larryzodiac <evanmachale@gmail.com>
2019-04-18 21:00:27 +0100

    Updates

---
+++ precache-manifest.8cc4da9fe9093ef086a4df957076f16c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4435a2b9c9ae6f5cee35",
+    "url": "/generative-jewellery-demo/static/js/runtime~main.c770ea03.js"
+  },
+  {
+    "revision": "65aec59af6de971d0ad4",
+    "url": "/generative-jewellery-demo/static/js/main.8a1901c5.chunk.js"
+  },
+  {
+    "revision": "31cef7d1426070f370a0",
+    "url": "/generative-jewellery-demo/static/js/2.51e10cc9.chunk.js"
+  },
+  {
+    "revision": "65aec59af6de971d0ad4",
+    "url": "/generative-jewellery-demo/static/css/main.ac6bc343.chunk.css"
+  },
+  {
+    "revision": "fc503650ab17eacd19aa48e0cea86b6d",
+    "url": "/generative-jewellery-demo/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/generative-jewellery-demo/precache-manifest.8cc4da9fe9093ef086a4df957076f16c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/generative-jewellery-demo/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
